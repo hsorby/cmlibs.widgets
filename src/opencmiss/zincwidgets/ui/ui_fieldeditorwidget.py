7ad3ae7ac1c31c2ae32ce6ca9f4b8f45177b2550
hsorby <h.sorby@auckland.ac.nz>
2022-03-16 22:11:46 +1300

    Fix bug where not setting a defined parameter raised an exception.

--- src/opencmiss/zincwidgets/ui/ui_fieldeditorwidget.py
+++ src/opencmiss/zincwidgets/ui/ui_fieldeditorwidget.py
@@ -31,33 +31,11 @@ class Ui_FieldEditorWidget(object):
         FieldEditorWidget.setMinimumSize(QSize(180, 0))
         self.gridLayout_2 = QGridLayout(FieldEditorWidget)
         self.gridLayout_2.setObjectName(u"gridLayout_2")
-        self.field_type_label = QLabel(FieldEditorWidget)
-        self.field_type_label.setObjectName(u"field_type_label")
-        sizePolicy1 = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Preferred)
-        sizePolicy1.setHorizontalStretch(0)
-        sizePolicy1.setVerticalStretch(0)
-        sizePolicy1.setHeightForWidth(self.field_type_label.sizePolicy().hasHeightForWidth())
-        self.field_type_label.setSizePolicy(sizePolicy1)
-        self.field_type_label.setMinimumSize(QSize(100, 27))
-
-        self.gridLayout_2.addWidget(self.field_type_label, 0, 0, 1, 1)
-
-        self.field_type_chooser = FieldTypeChooserWidget(FieldEditorWidget)
-        self.field_type_chooser.setObjectName(u"field_type_chooser")
-        sizePolicy2 = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
-        sizePolicy2.setHorizontalStretch(0)
-        sizePolicy2.setVerticalStretch(0)
-        sizePolicy2.setHeightForWidth(self.field_type_chooser.sizePolicy().hasHeightForWidth())
-        self.field_type_chooser.setSizePolicy(sizePolicy2)
-        self.field_type_chooser.setSizeAdjustPolicy(QComboBox.AdjustToContents)
-
-        self.gridLayout_2.addWidget(self.field_type_chooser, 0, 1, 1, 1)
-
         self.sourcefields_groupbox = QGroupBox(FieldEditorWidget)
         self.sourcefields_groupbox.setObjectName(u"sourcefields_groupbox")
         self.gridLayout_4 = QGridLayout(self.sourcefields_groupbox)
-        self.gridLayout_4.setContentsMargins(7, 7, 7, 7)
         self.gridLayout_4.setObjectName(u"gridLayout_4")
+        self.gridLayout_4.setContentsMargins(7, 7, 7, 7)
         self.number_of_source_fields_label = QLabel(self.sourcefields_groupbox)
         self.number_of_source_fields_label.setObjectName(u"number_of_source_fields_label")
 
@@ -82,16 +60,34 @@ class Ui_FieldEditorWidget(object):
 
         self.gridLayout_2.addWidget(self.sourcefields_groupbox, 3, 0, 1, 2)
 
-        self.applyargumentfields_groupbox = QGroupBox(FieldEditorWidget)
-        self.applyargumentfields_groupbox.setObjectName(u"applyargumentfields_groupbox")
-        self.gridLayout_11 = QGridLayout(self.applyargumentfields_groupbox)
-        self.gridLayout_11.setObjectName(u"gridLayout_11")
+        self.field_groupbox = QGroupBox(FieldEditorWidget)
+        self.field_groupbox.setObjectName(u"field_groupbox")
+        self.horizontalLayout = QHBoxLayout(self.field_groupbox)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.field_type_label = QLabel(self.field_groupbox)
+        self.field_type_label.setObjectName(u"field_type_label")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Preferred)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.field_type_label.sizePolicy().hasHeightForWidth())
+        self.field_type_label.setSizePolicy(sizePolicy1)
+        self.field_type_label.setMinimumSize(QSize(100, 27))
 
-        self.gridLayout_2.addWidget(self.applyargumentfields_groupbox, 5, 0, 1, 2)
+        self.horizontalLayout.addWidget(self.field_type_label)
 
-        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
+        self.field_type_chooser = FieldTypeChooserWidget(self.field_groupbox)
+        self.field_type_chooser.setObjectName(u"field_type_chooser")
+        sizePolicy2 = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        sizePolicy2.setHorizontalStretch(0)
+        sizePolicy2.setVerticalStretch(0)
+        sizePolicy2.setHeightForWidth(self.field_type_chooser.sizePolicy().hasHeightForWidth())
+        self.field_type_chooser.setSizePolicy(sizePolicy2)
+        self.field_type_chooser.setSizeAdjustPolicy(QComboBox.AdjustToContents)
 
-        self.gridLayout_2.addItem(self.verticalSpacer, 7, 0, 1, 1)
+        self.horizontalLayout.addWidget(self.field_type_chooser)
+
+
+        self.gridLayout_2.addWidget(self.field_groupbox, 0, 0, 1, 1)
 
         self.coordinate_system_groupbox = QGroupBox(FieldEditorWidget)
         self.coordinate_system_groupbox.setObjectName(u"coordinate_system_groupbox")
@@ -178,6 +174,17 @@ class Ui_FieldEditorWidget(object):
 
         self.gridLayout_2.addWidget(self.derived_groupbox, 4, 0, 1, 2)
 
+        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
+
+        self.gridLayout_2.addItem(self.verticalSpacer, 7, 0, 1, 1)
+
+        self.applyargumentfields_groupbox = QGroupBox(FieldEditorWidget)
+        self.applyargumentfields_groupbox.setObjectName(u"applyargumentfields_groupbox")
+        self.gridLayout_11 = QGridLayout(self.applyargumentfields_groupbox)
+        self.gridLayout_11.setObjectName(u"gridLayout_11")
+
+        self.gridLayout_2.addWidget(self.applyargumentfields_groupbox, 5, 0, 1, 2)
+
         self.general_groupbox = QGroupBox(FieldEditorWidget)
         self.general_groupbox.setObjectName(u"general_groupbox")
         self.general_groupbox.setEnabled(True)
@@ -198,28 +205,41 @@ class Ui_FieldEditorWidget(object):
 
         self.gridLayout_2.addWidget(self.general_groupbox, 1, 0, 1, 2)
 
-        self.create_group = QGroupBox(FieldEditorWidget)
-        self.create_group.setObjectName(u"create_group")
-        self.create_group.setMinimumSize(QSize(180, 0))
-        self.formLayout = QFormLayout(self.create_group)
+        self.create_groupbox = QGroupBox(FieldEditorWidget)
+        self.create_groupbox.setObjectName(u"create_groupbox")
+        self.create_groupbox.setMinimumSize(QSize(180, 0))
+        self.formLayout = QFormLayout(self.create_groupbox)
         self.formLayout.setObjectName(u"formLayout")
-        self.name_label = QLabel(self.create_group)
+        self.name_label = QLabel(self.create_groupbox)
         self.name_label.setObjectName(u"name_label")
 
         self.formLayout.setWidget(0, QFormLayout.LabelRole, self.name_label)
 
-        self.create_button = QPushButton(self.create_group)
-        self.create_button.setObjectName(u"create_button")
-
-        self.formLayout.setWidget(1, QFormLayout.SpanningRole, self.create_button)
-
-        self.name_lineedit = QLineEdit(self.create_group)
+        self.name_lineedit = QLineEdit(self.create_groupbox)
         self.name_lineedit.setObjectName(u"name_lineedit")
 
         self.formLayout.setWidget(0, QFormLayout.FieldRole, self.name_lineedit)
 
+        self.horizontalLayout_2 = QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.horizontalSpacer = QSpacerItem(118, 17, QSizePolicy.Expanding, QSizePolicy.Minimum)
 
-        self.gridLayout_2.addWidget(self.create_group, 6, 0, 1, 2)
+        self.horizontalLayout_2.addItem(self.horizontalSpacer)
+
+        self.create_button = QPushButton(self.create_groupbox)
+        self.create_button.setObjectName(u"create_button")
+
+        self.horizontalLayout_2.addWidget(self.create_button)
+
+        self.horizontalSpacer_2 = QSpacerItem(122, 17, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout_2.addItem(self.horizontalSpacer_2)
+
+
+        self.formLayout.setLayout(1, QFormLayout.SpanningRole, self.horizontalLayout_2)
+
+
+        self.gridLayout_2.addWidget(self.create_groupbox, 6, 0, 1, 2)
 
 
         self.retranslateUi(FieldEditorWidget)
@@ -229,12 +249,12 @@ class Ui_FieldEditorWidget(object):
 
     def retranslateUi(self, FieldEditorWidget):
         FieldEditorWidget.setWindowTitle(QCoreApplication.translate("FieldEditorWidget", u"Field Editor", None))
-        self.field_type_label.setText(QCoreApplication.translate("FieldEditorWidget", u"Field type:", None))
         self.sourcefields_groupbox.setTitle(QCoreApplication.translate("FieldEditorWidget", u"Source fields:", None))
         self.number_of_source_fields_label.setText(QCoreApplication.translate("FieldEditorWidget", u"Number of source fields:", None))
         self.region_of_apply_fields_label.setText(QCoreApplication.translate("FieldEditorWidget", u"Region of evaluate field:", None))
-        self.applyargumentfields_groupbox.setTitle(QCoreApplication.translate("FieldEditorWidget", u"Apply argument fields:", None))
-        self.coordinate_system_groupbox.setTitle(QCoreApplication.translate("FieldEditorWidget", u"Coordinate System:", None))
+        self.field_groupbox.setTitle(QCoreApplication.translate("FieldEditorWidget", u"Field:", None))
+        self.field_type_label.setText(QCoreApplication.translate("FieldEditorWidget", u"Type:", None))
+        self.coordinate_system_groupbox.setTitle(QCoreApplication.translate("FieldEditorWidget", u"Coordinate system:", None))
         self.coordinate_system_type_label.setText(QCoreApplication.translate("FieldEditorWidget", u"Type:", None))
         self.coordinate_system_type_chooser.setItemText(0, QCoreApplication.translate("FieldEditorWidget", u"Rectangular Cartesian", None))
         self.coordinate_system_type_chooser.setItemText(1, QCoreApplication.translate("FieldEditorWidget", u"Cylindrial Polar", None))
@@ -244,15 +264,16 @@ class Ui_FieldEditorWidget(object):
         self.coordinate_system_type_chooser.setItemText(5, QCoreApplication.translate("FieldEditorWidget", u"Fibre", None))
 
         self.coordinate_system_focus_label.setText(QCoreApplication.translate("FieldEditorWidget", u"Focus:", None))
-        self.derived_groupbox.setTitle(QCoreApplication.translate("FieldEditorWidget", u"Derived Parameters:", None))
+        self.derived_groupbox.setTitle(QCoreApplication.translate("FieldEditorWidget", u"Derived parameters:", None))
         self.derived_combo_label_1.setText(QCoreApplication.translate("FieldEditorWidget", u"Derived combo1", None))
         self.derived_values_label.setText(QCoreApplication.translate("FieldEditorWidget", u"Constant Values:", None))
         self.derived_combo_label_2.setText(QCoreApplication.translate("FieldEditorWidget", u"Derived combo2", None))
         self.derived_combo_label_3.setText(QCoreApplication.translate("FieldEditorWidget", u"Derived combo3", None))
-        self.general_groupbox.setTitle("")
+        self.applyargumentfields_groupbox.setTitle(QCoreApplication.translate("FieldEditorWidget", u"Apply argument fields:", None))
+        self.general_groupbox.setTitle(QCoreApplication.translate("FieldEditorWidget", u"Properties:", None))
         self.type_coordinate_checkbox.setText(QCoreApplication.translate("FieldEditorWidget", u"Is Coordinate", None))
         self.managed_checkbox.setText(QCoreApplication.translate("FieldEditorWidget", u"Managed", None))
-        self.create_group.setTitle("")
+        self.create_groupbox.setTitle(QCoreApplication.translate("FieldEditorWidget", u"Create:", None))
         self.name_label.setText(QCoreApplication.translate("FieldEditorWidget", u"Name:", None))
         self.create_button.setText(QCoreApplication.translate("FieldEditorWidget", u"Create", None))
     # retranslateUi
