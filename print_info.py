72dbb6fd20ed7b0735275967a152f159e5bb4ce1
Hugh Sorby <h.sorby@auckland.ac.nz>
2024-06-28 18:04:40 +1200

    Investigate PySide6 on Linux.

---
+++ print_info.py
@@ -0,0 +1,11 @@
+import PySide6
+from PySide6 import QtCore
+
+app = QtCore.QCoreApplication([])
+info = QtCore.QLibraryInfo()
+print("Package paths:", PySide6.__path__)
+print("Prefix path:", info.path(QtCore.QLibraryInfo.LibraryPath.PrefixPath))
+print("Binaries path:", info.path(QtCore.QLibraryInfo.LibraryPath.BinariesPath))
+print("Library executables path:", info.path(QtCore.QLibraryInfo.LibraryPath.LibraryExecutablesPath))
+print("Plugins path:", info.path(QtCore.QLibraryInfo.LibraryPath.PluginsPath))
+print("")
